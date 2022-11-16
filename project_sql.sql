d89e8a9cc7bf61d477a3cd42c321d9c256010a8e
akw2000 <akwarnakulasuriya@GMAIL.COM>
2022-11-16 22:03:45 +0530

    sql updated

--- project_sql.sql
+++ project_sql.sql
@@ -1,8 +1,8 @@
 CREATE TABLE product_variant
 	(sku			CHAR(8), 
 	 name			VARCHAR(50),
-	 inventory_id	INT(7) NOT NULL AUTO_INCREMENT, 
-	 product_id		INT(7) NOT NULL AUTO_INCREMENT,
+	 inventory_id	INT(7) NOT NULL, 
+	 product_id		INT(7) NOT NULL,
 	 price			NUMERIC(10,2),
 	 image_url		VARCHAR(255),
 	 PRIMARY KEY (sku),
@@ -15,7 +15,7 @@ CREATE TABLE product_variant
 CREATE TABLE product_variant_option
 	(id			    INT(7) NOT NULL AUTO_INCREMENT, 
 	 sku			CHAR(8),
-	 option_id	    INT(7) NOT NULL AUTO_INCREMENT, 
+	 option_id	    INT(7) NOT NULL, 
 	 PRIMARY KEY (id),
 	 FOREIGN KEY (sku) REFERENCES product_variant(sku)
 		ON DELETE SET NULL,
@@ -28,7 +28,7 @@ CREATE TABLE product
 	 name			VARCHAR(50),
 	 description	VARCHAR(100), 
      base_price		NUMERIC(10,2),
-     discount_id	INT(7) NOT NULL AUTO_INCREMENT,
+     discount_id	INT(7) NOT NULL,
 	 brand		    VARCHAR(50), 
 	 PRIMARY KEY (id),
 	 FOREIGN KEY (discount_id) REFERENCES discount(id)
@@ -37,8 +37,8 @@ CREATE TABLE product
 
 CREATE TABLE product_sub_category
 	(id			    INT(7) NOT NULL AUTO_INCREMENT, 
-	 product_id	    INT(7) NOT NULL AUTO_INCREMENT, 
-	 subcategory_id INT(7) NOT NULL AUTO_INCREMENT, 
+	 product_id	    INT(7) NOT NULL, 
+	 subcategory_id INT(7) NOT NULL, 
 	 PRIMARY KEY (id),
 	 FOREIGN KEY (product_id) REFERENCES product(id)
 		ON DELETE SET NULL,
