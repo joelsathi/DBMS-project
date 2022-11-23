90eacf0699fdc319209e626db0df7cae575aa24e
Sandil Ranasinghe <sandilsranasinghe@gmail.com>
2022-11-23 17:35:56 +0530

    simple express application init

--- backend/app.js
+++ backend/app.js
@@ -1,36 +1,18 @@
-const http = require("http");
 
-const hostname = "127.0.0.1";
-const port = 3000;
+const express = require("express")
+const app = express()
+const host = "localhost"
+const port = 3001
 
-const server = http.createServer((req, res) => {
-  var url = req.url;
-  switch (url) {
-    case "/":
-      res.statusCode = 200;
-      res.setHeader("Content-Type", "text/html");
-      res.write("<h1>Welcome to Thulasi Stores</h1>");
-      res.end();
-      break;
+const router = express.Router()
+router.get("/", (req, res) => {
+  res.send("Hello World")
+})
+router.get("/about", (req, res) => {
+  res.json({projectName: "Thulasi", description: "Some Description"})
+})
+app.use(router)
 
-    case "/about":
-      res.statusCode = 200;
-      res.setHeader("Content-Type", "text/html");
-      res.write(
-        "This is the project thulasi stores which we are doing for dbms"
-      );
-      res.end("");
-      break;
-
-    default:
-      res.statusCode = 404;
-      res.setHeader("Content-Type", "text/html");
-      res.write("Page Not Found");
-      res.end();
-      break;
-  }
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port, host, () => {
+  console.log(`Server running on port ${port}`)
+})
