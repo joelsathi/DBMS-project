f03712c9359f3612f1a28fdffa0ee5f88c356739
Sandil Ranasinghe <sandilsranasinghe@gmail.com>
2022-11-29 10:01:32 +0530

    install mysql2 and config connection

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,15 @@
 import express, { Request, Response } from "express";
+import appConfig from "./config";
+import mysql2, { Connection } from "mysql2";
+
+const db: Connection = mysql2.createConnection(appConfig.DB_CONFIG);
+db.connect((err: any) => {
+  if (err) {
+    throw err
+  } else {
+    console.log("Connected to mysql database")
+  }
+})
 
 const router = express.Router();
 router.get("/", (req: Request, res: Response) => {
@@ -10,8 +21,6 @@ router.get("/about", (req: Request, res: Response) => {
 
 const app = express();
 app.use(router);
-const host = "localhost";
-const port = 3001;
-app.listen(port, host, () => {
-  console.log(`Server running on port ${port}`);
+app.listen(appConfig.PORT, appConfig.HOST, () => {
+  console.log(`Server running on port ${appConfig.PORT}`);
 });
---
+++ backend/src/config.ts
@@ -0,0 +1,27 @@
+import * as dotenv from "dotenv";
+import { ConnectionOptions } from "mysql2"
+
+dotenv.config({path: __dirname+"/../.env"})
+
+interface AppConfig {
+    HOST: string,
+    PORT: number,
+    NODE_ENV: string,
+    DB_CONFIG: ConnectionOptions,
+    DB_NAME: string
+}
+
+const appConfig: AppConfig = {
+    HOST: process.env.HOST || "localhost",
+    PORT: parseInt(process.env.PORT) || 3001,
+    NODE_ENV: process.env.NODE_ENV || "DEV",
+    DB_NAME: process.env.DB_NAME,
+    DB_CONFIG: {
+        port: parseInt(process.env.DB_PORT) || 3306,
+        host: process.env.DB_HOST || "127.0.0.1",
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        connectionLimit: parseInt(process.env.DB_CONNECTION_LIMIT) || 2
+    }
+}
+export default appConfig
