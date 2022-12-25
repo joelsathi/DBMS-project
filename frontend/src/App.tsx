c5be68c786b797e02962306a84839e3b154627fb
joelsathi <joelsathiyendra@gmai.com>
2022-12-25 20:30:19 +0530

    Added Single Product Page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,29 @@
-import { Admin, Resource, ListGuesser } from "react-admin";
+import { Admin, Resource, ListGuesser, CustomRoutes } from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
+import { Route } from "react-router-dom";
+
+
+import { Checkout, SingleProduct, Testing } from "./container";
+
+// const addnums = (num1: number, num2: number) => { return num1 + num2}
 
 const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
 
 const App = () => (
-  <Admin dataProvider={dataProvider}>
-    <Resource name="posts" list={ListGuesser} />
-    <Resource name="comments" list={ListGuesser} />
-  </Admin>
+  // <SingleProduct />
+  // <Admin dataProvider={dataProvider}>
+  //   {/* <Resource name="posts" list={ListGuesser} />
+  //   <Resource name="comments" list={ListGuesser} /> */}
+  //   {/* <Route path="/Checkout" element={<SingleProduct/>}/> */}
+    
+  // </Admin>
+  // <CustomRoutes>
+  // {/* <Route path="/Checkout" element={<Checkout/>} /> */}
+  //   <Route path="/Checkout" element={<SingleProduct/>} />
+  // </CustomRoutes>
+  <SingleProduct />
+  // <Checkout />
+  // <Testing />
 );
 
 export default App;
