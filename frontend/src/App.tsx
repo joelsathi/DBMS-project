0aaba6d47ed72928ec22586ea16853eb591a46f0
joelsathi <joelsathiyendra@gmai.com>
2023-01-05 09:52:59 +0530

    Moved URL out of the app and placed in constants

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,22 @@
-import { Admin, Resource, ListGuesser, CustomRoutes } from "react-admin";
+import { Admin, Resource, ListGuesser, CustomRoutes, convertLegacyDataProvider } from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
 import { Route } from "react-router-dom";
 
+import dataProvider from "./api/dataProvider";
 
-import { Checkout, SingleProduct, Testing } from "./container";
+import { SingleProduct } from "./container";
 
-// const addnums = (num1: number, num2: number) => { return num1 + num2}
-
-const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
+// import { Testing } from "./container";
 
 const App = () => (
-  // <SingleProduct />
-  // <Admin dataProvider={dataProvider}>
-  //   {/* <Resource name="posts" list={ListGuesser} />
-  //   <Resource name="comments" list={ListGuesser} /> */}
-  //   {/* <Route path="/Checkout" element={<SingleProduct/>}/> */}
-    
-  // </Admin>
-  // <CustomRoutes>
-  // {/* <Route path="/Checkout" element={<Checkout/>} /> */}
-  //   <Route path="/Checkout" element={<SingleProduct/>} />
-  // </CustomRoutes>
-  <SingleProduct />
-  // <Checkout />
-  // <Testing />
+  <Admin dataProvider={dataProvider}>
+    <Resource name="posts" list={ListGuesser} />
+    <Resource name="comments" list={ListGuesser} />
+  <CustomRoutes>
+    <Route path="/Checkout" element={<SingleProduct/>}/>
+    {/* <Route path="/testingDP" element={<Testing/>}/> */}
+  </CustomRoutes>
+  </Admin>
 );
 
 export default App;
