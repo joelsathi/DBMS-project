32b1630a6727ea2249d182ad31a6b4df89551ced
Kavindu Warnakulasuriya <86293207+akw2000@users.noreply.github.com>
2023-01-05 21:12:47 +0530

    new folder frontend_vite

---
+++ frontend_vite/src/utils/helper.ts
@@ -0,0 +1,22 @@
+const CURRENCRY_FORMATTER = new Intl.NumberFormat(undefined, {
+  currency: 'USD',
+  style: 'currency',
+});
+
+export const formatCurrencry = (number: any) => {
+  return CURRENCRY_FORMATTER.format(number);
+};
+
+export const getDate = (date: Date) => {
+  return new Date(date).toLocaleDateString('en');
+};
+// backend server url(replace with fastapi server url)
+export const baseUrl =
+  import.meta.env.VITE_MODE === 'development'
+    ? 'http://localhost:5000'
+    : 'https://typeshop-server.onrender.com';
+// to check with a empty database
+// export const baseUrl =
+//   import.meta.env.VITE_MODE === 'development'
+//     ? ''
+//     : '';
