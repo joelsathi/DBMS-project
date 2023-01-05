9bcbae6952f34f42de67050702b2e61fcec1fbb4
Kavindu Warnakulasuriya <86293207+akw2000@users.noreply.github.com>
2023-01-05 13:11:24 +0530

    initial working frontend with npm run dev

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,19 +1,40 @@
+// npm start not working
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { persistor, store } from './redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
+// ReactDOM.createRoot(document.getElementById('root')!).render(
+//   <Provider store={store}>
+//     <PersistGate loading={null} persistor={persistor}>
+//       <App />
+//     </PersistGate>
+//   </Provider>
+// );
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
   <React.StrictMode>
+  <Provider store={store}>
+  <PersistGate loading={null} persistor={persistor}>
     <App />
+  </PersistGate>
+</Provider>
   </React.StrictMode>
+      
+
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
