4cbe737b774a47ed4f97b683248c5aaab314cae5
joelsathi <joelsathiyendra@gmai.com>
2023-01-05 10:06:15 +0530

    testing DP

---
+++ frontend/src/container/index.js
@@ -0,0 +1,3 @@
+export {default as SingleProduct} from './SingleProduct/SingleProduct'
+export {default as Testing} from './Testing/testing'
+// export {default as Checkout} from './Checkout/Checkout'
