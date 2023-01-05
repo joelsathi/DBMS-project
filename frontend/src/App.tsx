56f7b3d0f41181b29c2ab8bb338425a73d929989
joelsathi <joelsathiyendra@gmai.com>
2023-01-05 09:52:59 +0530

    Moved URL out of the app and placed in constants

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,21 @@
-import { Admin, Resource, ListGuesser } from "react-admin";
+import { Admin, Resource, ListGuesser, CustomRoutes, convertLegacyDataProvider } from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
+import { Route } from "react-router-dom";
 
-const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
+import dataProvider from "./api/dataProvider";
+
+import { SingleProduct } from "./container";
+
+// import { Testing } from "./container";
 
 const App = () => (
   <Admin dataProvider={dataProvider}>
     <Resource name="posts" list={ListGuesser} />
     <Resource name="comments" list={ListGuesser} />
+  <CustomRoutes>
+    <Route path="/Checkout" element={<SingleProduct/>}/>
+    {/* <Route path="/testingDP" element={<Testing/>}/> */}
+  </CustomRoutes>
   </Admin>
 );
 
