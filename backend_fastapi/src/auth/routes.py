849aebf6b474cd11b776b41753d0b12f72b54c9b
joelsathi <joelsathiyendra@gmai.com>
2023-01-07 00:25:45 +0530

    Implmented Pagination for Reg_user

--- backend_fastapi/src/auth/routes.py
+++ backend_fastapi/src/auth/routes.py
@@ -8,10 +8,38 @@ user_router = APIRouter(
 
 
 @user_router.get("/registered_user")
-def get_registered_user_list(response: Response):
-    rows = RegisteredUserDBModel.objects.select()
+def get_registered_user_list(response: Response, page_num: int = 1, page_size: int = 10):
+    rows = RegisteredUserDBModel.objects.select_by_page(page_num=page_num, page_size=page_size)
+    total = 18
     serialized_rows = [RegisteredUserDBModel.serialize(row) for row in rows]
-    return serialized_rows
+    
+    start = (page_num - 1) * page_size
+    end = start + page_size
+
+    ret = {
+        "data": serialized_rows,
+        "total": total,
+        "count": page_size,
+        "pagination": {}
+    }
+
+    if end >= total:
+        ret["pagination"]["next"] = None
+
+        if page_num > 1:
+            ret["pagination"]["previous"] = f"/registered_user?page_num={page_num-1}&page_size={page_size}"
+        else:
+            ret["pagination"]["previous"] = None
+    else:
+        if page_num > 1:
+            ret["pagination"]["previous"] = f"/registered_user?page_num={page_num-1}&page_size={page_size}"
+        else:
+            ret["pagination"]["previous"] = None
+
+        ret["pagination"]["next"] = f"/registered_user?page_num={page_num+1}&page_size={page_size}"
+
+    return ret
+    # return serialized_rows
 
 
 @user_router.get("/registered_user/{id}")
