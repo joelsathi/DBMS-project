3cc95fa05eae62d69e5856cd8849ba06d8c9d5ed
joelsathi <joelsathiyendra@gmai.com>
2023-01-07 11:34:20 +0530

    Changed the non implemented total to None

--- backend_fastapi/src/auth/routes.py
+++ backend_fastapi/src/auth/routes.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, Response, status
 
 from .models import RegisteredUserDBModel, UserDBModel, PaymentDetailDBModel
 
+from ..core.pagination import getPagination
+
 user_router = APIRouter(
     prefix="/auth",
 )
@@ -10,36 +12,12 @@ user_router = APIRouter(
 @user_router.get("/registered_user")
 def get_registered_user_list(response: Response, page_num: int = 1, page_size: int = 10):
     rows = RegisteredUserDBModel.objects.select_by_page(page_num=page_num, page_size=page_size)
-    total = 18
+    total = None # NEED TO IMPLEMENT THE FUNCTION
     serialized_rows = [RegisteredUserDBModel.serialize(row) for row in rows]
-    
-    start = (page_num - 1) * page_size
-    end = start + page_size
 
-    ret = {
-        "data": serialized_rows,
-        "total": total,
-        "count": page_size,
-        "pagination": {}
-    }
-
-    if end >= total:
-        ret["pagination"]["next"] = None
-
-        if page_num > 1:
-            ret["pagination"]["previous"] = f"/registered_user?page_num={page_num-1}&page_size={page_size}"
-        else:
-            ret["pagination"]["previous"] = None
-    else:
-        if page_num > 1:
-            ret["pagination"]["previous"] = f"/registered_user?page_num={page_num-1}&page_size={page_size}"
-        else:
-            ret["pagination"]["previous"] = None
-
-        ret["pagination"]["next"] = f"/registered_user?page_num={page_num+1}&page_size={page_size}"
+    ret = getPagination("/registered_user", total=total, serialized_rows=serialized_rows, page_num=page_num, page_size=page_size)
 
     return ret
-    # return serialized_rows
 
 
 @user_router.get("/registered_user/{id}")
