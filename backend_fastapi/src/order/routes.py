2144ee45c24d9b2bbb0ffd01d22cd5029be17675
joelsathi <joelsathiyendra@gmai.com>
2023-01-07 11:51:02 +0530

    Added pagination for the rest of the routes

--- backend_fastapi/src/order/routes.py
+++ backend_fastapi/src/order/routes.py
@@ -7,41 +7,53 @@ from .models import (
     LocationModel,
 )
 
+from ..core.pagination import getPagination
+
 order_router = APIRouter(
     prefix="/order",
 )
 
 
 @order_router.get("/order_cart")
-def get_order_cart_list(response: Response):
-    rows = OrderCartModel.objects.select()
+def get_order_cart_list(response: Response, page_num: int = 1, page_size: int = 10):
+    rows = OrderCartModel.objects.select_by_page(page_num=page_num, page_size=page_size)
+    total = None # NEED TO IMPLEMENT THE FUNCTION
     serialized_rows = [OrderCartModel.serialize(row) for row in rows]
-    return serialized_rows
+    ret = getPagination("/order_cart", total=total, serialized_rows=serialized_rows, page_num=page_num, page_size=page_size)
+    return ret
 
 
 @order_router.get("/product_order")
-def get_product_order_list(response: Response):
-    rows = ProductOrderModel.objects.select()
+def get_product_order_list(response: Response, page_num: int = 1, page_size: int = 10):
+    rows = ProductOrderModel.objects.select_by_page(page_num=page_num, page_size=page_size)
+    total = None # NEED TO IMPLEMENT THE FUNCTION
     serialized_rows = [ProductOrderModel.serialize(row) for row in rows]
-    return serialized_rows
+    ret = getPagination("/product_order", total=total, serialized_rows=serialized_rows, page_num=page_num, page_size=page_size)
+    return ret
 
 
 @order_router.get("/order_payment_detail")
-def get_order_payment_detail_list(response: Response):
-    rows = OrderPaymentDetailModel.objects.select()
+def get_order_payment_detail_list(response: Response, page_num: int = 1, page_size: int = 10):
+    rows = OrderPaymentDetailModel.objects.select_by_page(page_num=page_num, page_size=page_size)
+    total = None # NEED TO IMPLEMENT THE FUNCTION
     serialized_rows = [OrderPaymentDetailModel.serialize(row) for row in rows]
-    return serialized_rows
+    ret = getPagination("/order_payment_detail", total=total, serialized_rows=serialized_rows, page_num=page_num, page_size=page_size)
+    return ret
 
 
 @order_router.get("/delivery")
-def get_delivery_list(response: Response):
-    rows = DeliveryModel.objects.select()
+def get_delivery_list(response: Response, page_num: int = 1, page_size: int = 10):
+    rows = DeliveryModel.objects.select_by_page(page_num=page_num, page_size=page_size)
+    total = None # NEED TO IMPLEMENT THE FUNCTION
     serialized_rows = [DeliveryModel.serialize(row) for row in rows]
-    return serialized_rows
+    ret = getPagination("/delivery", total=total, serialized_rows=serialized_rows, page_num=page_num, page_size=page_size)
+    return ret
 
 
 @order_router.get("/location")
-def get_location_list(response: Response):
-    rows = LocationModel.objects.select()
+def get_location_list(response: Response, page_num: int = 1, page_size: int = 10):
+    rows = LocationModel.objects.select_by_page(page_num=page_num, page_size=page_size)
+    total = None # NEED TO IMPLEMENT THE FUNCTION
     serialized_rows = [LocationModel.serialize(row) for row in rows]
-    return serialized_rows
+    ret = getPagination("/location", total=total, serialized_rows=serialized_rows, page_num=page_num, page_size=page_size)
+    return ret
--- backend_fastapi/src/product/routes.py
+++ backend_fastapi/src/product/routes.py
@@ -12,69 +12,89 @@ from .models import (
     ProductVarientOptionsModel,
 )
 
+from ..core.pagination import getPagination
+
 product_router = APIRouter(
     prefix="/product",
 )
 
 
 @product_router.get("/product")
-def get_product_list(response: Response):
-    rows = ProductModel.objects.select()
+def get_product_list(response: Response, page_num: int = 1, page_size: int = 10):
+    rows = ProductModel.objects.select_by_page(page_num=page_num, page_size=page_size)
+    total = None # NEED TO IMPLEMENT THE FUNCTION
     serialized_rows = [ProductModel.serialize(row) for row in rows]
-    return serialized_rows
+    ret = getPagination("/product", total=total, serialized_rows=serialized_rows, page_num=page_num, page_size=page_size)
+    return ret
 
 
 @product_router.get("/variant")
-def get_variant_list(response: Response):
-    rows = ProductVariantModel.objects.select()
+def get_variant_list(response: Response, page_num: int = 1, page_size: int = 10):
+    rows = ProductVariantModel.objects.select_by_page(page_num=page_num, page_size=page_size)
+    total = None # NEED TO IMPLEMENT THE FUNCTION
     serialized_rows = [ProductVariantModel.serialize(row) for row in rows]
-    return serialized_rows
+    ret = getPagination("/variant", total=total, serialized_rows=serialized_rows, page_num=page_num, page_size=page_size)
+    return ret
 
 
 @product_router.get("/subcategory")
-def get_subcategory_list(response: Response):
-    rows = SubCategoryModel.objects.select()
+def get_subcategory_list(response: Response, page_num: int = 1, page_size: int = 10):
+    rows = SubCategoryModel.objects.select_by_page(page_num=page_num, page_size=page_size)
+    total = None # NEED TO IMPLEMENT THE FUNCTION
     serialized_rows = [SubCategoryModel.serialize(row) for row in rows]
-    return serialized_rows
+    ret = getPagination("/subcategory", total=total, serialized_rows=serialized_rows, page_num=page_num, page_size=page_size)
+    return ret
 
 
 @product_router.get("/supercategory")
-def get_supercategory_list(response: Response):
-    rows = SuperCategoryModel.objects.select()
+def get_supercategory_list(response: Response, page_num: int = 1, page_size: int = 10):
+    rows = SuperCategoryModel.objects.select_by_page(page_num=page_num, page_size=page_size)
+    total = None # NEED TO IMPLEMENT THE FUNCTION
     serialized_rows = [SuperCategoryModel.serialize(row) for row in rows]
-    return serialized_rows
+    ret = getPagination("/supercategory", total=total, serialized_rows=serialized_rows, page_num=page_num, page_size=page_size)
+    return ret
 
 
 @product_router.get("/discount")
-def get_discount_list(response: Response):
-    rows = DiscountModel.objects.select()
+def get_discount_list(response: Response, page_num: int = 1, page_size: int = 10):
+    rows = DiscountModel.objects.select_by_page(page_num=page_num, page_size=page_size)
+    total = None # NEED TO IMPLEMENT THE FUNCTION
     serialized_rows = [DiscountModel.serialize(row) for row in rows]
-    return serialized_rows
+    ret = getPagination("/discount", total=total, serialized_rows=serialized_rows, page_num=page_num, page_size=page_size)
+    return ret
 
 
 @product_router.get("/options")
-def get_options_list(response: Response):
-    rows = OptionsModel.objects.select()
+def get_options_list(response: Response, page_num: int = 1, page_size: int = 10):
+    rows = OptionsModel.objects.select_by_page(page_num=page_num, page_size=page_size)
+    total = None # NEED TO IMPLEMENT THE FUNCTION
     serialized_rows = [OptionsModel.serialize(row) for row in rows]
-    return serialized_rows
+    ret = getPagination("/options", total=total, serialized_rows=serialized_rows, page_num=page_num, page_size=page_size)
+    return ret
 
 
 @product_router.get("/inventory")
-def get_inventory_list(response: Response):
-    rows = InventoryModel.objects.select()
+def get_inventory_list(response: Response, page_num: int = 1, page_size: int = 10):
+    rows = InventoryModel.objects.select_by_page(page_num=page_num, page_size=page_size)
+    total = None # NEED TO IMPLEMENT THE FUNCTION
     serialized_rows = [InventoryModel.serialize(row) for row in rows]
-    return serialized_rows
+    ret = getPagination("/inventory", total=total, serialized_rows=serialized_rows, page_num=page_num, page_size=page_size)
+    return ret
 
 
 @product_router.get("/product_subcategory")
-def get_product_subcategory_list(response: Response):
-    rows = ProductSubCategoryModel.objects.select()
+def get_product_subcategory_list(response: Response, page_num: int = 1, page_size: int = 10):
+    rows = ProductSubCategoryModel.objects.select_by_page(page_num=page_num, page_size=page_size)
+    total = None # NEED TO IMPLEMENT THE FUNCTION
     serialized_rows = [ProductSubCategoryModel.serialize(row) for row in rows]
-    return serialized_rows
+    ret = getPagination("/product_subcategory", total=total, serialized_rows=serialized_rows, page_num=page_num, page_size=page_size)
+    return ret
 
 
 @product_router.get("/product_variant_options")
-def get_product_variant_options_list(response: Response):
-    rows = ProductVarientOptionsModel.objects.select()
+def get_product_variant_options_list(response: Response, page_num: int = 1, page_size: int = 10):
+    rows = ProductVarientOptionsModel.objects.select_by_page(page_num=page_num, page_size=page_size)
+    total = None # NEED TO IMPLEMENT THE FUNCTION
     serialized_rows = [ProductVarientOptionsModel.serialize(row) for row in rows]
-    return serialized_rows
+    ret = getPagination("/product_variant_options", total=total, serialized_rows=serialized_rows, page_num=page_num, page_size=page_size)
+    return ret
