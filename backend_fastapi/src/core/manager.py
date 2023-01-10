3fd4e95011f602fb1134e18673d84ca5b05d0f7c
joelsathi <joelsathiyendra@gmai.com>
2023-01-10 20:56:28 +0530

    Function added for where clause Finished the where method

--- backend_fastapi/src/core/manager.py
+++ backend_fastapi/src/core/manager.py
@@ -115,6 +115,33 @@ class BaseQueryManager:
 
         return field_str
 
+    def _get_where_clause(self, filters: dict):
+        _field_names = self.model_class.get_field_names()
+
+        field_names = list(filters.keys())
+
+        # If there are no filtering just return an empty string
+        if len(field_names) == 0:
+            where_clause = ""
+
+        else:
+            where_clause = "WHERE "
+            for ind, field_name in enumerate(field_names):
+                # Throw an exception if the field names are not valid
+                if field_name not in _field_names:
+                    raise InvalidFieldException(
+                        "No field named {} found on {}".format(
+                            field_name, self.model_class.__qualname__
+                        )
+                    )
+                else:
+                    val_list = ",".join(filters[field_name])
+                    where_clause += f"{field_name} IN ({val_list})"
+                    if ind < len(field_names) - 1:
+                        where_clause += " AND "
+
+        return where_clause
+
     def select(self, field_names: list = [], filters: dict = None):
         # TODO implement filters (WHERE queries), limits, sorting (ORDER BY queries)
 
@@ -150,7 +177,7 @@ class BaseQueryManager:
         cursor.close()
 
         return row
-    
+
     # def get_count(self):
 
     #     sql_query_str = "SELECT COUNT(*) FROM {}".format(
@@ -165,13 +192,19 @@ class BaseQueryManager:
     #     cursor.close()
 
     #     return count
-    
-    def select_by_page(self, field_names: list = [], page_num: int = 1, page_size: int = 10, filters: dict = None):
+
+    def select_by_page(
+        self,
+        field_names: list = [],
+        page_num: int = 1,
+        page_size: int = 10,
+        filters: dict = None,
+    ):
         # TODO implement filters (WHERE queries), limits, sorting (ORDER BY queries)
 
         field_str = self._get_field_names_str(field_names)
 
-        start = (page_num - 1)*page_size
+        start = (page_num - 1) * page_size
 
         sql_query_str = "SELECT {} FROM {} LIMIT {} OFFSET {}".format(
             field_str, self.model_class.__tablename__, page_size, start
@@ -185,37 +218,37 @@ class BaseQueryManager:
 
         return rows
 
-    def select_by_all(self, field_names: list = [], page_num: int = 1, page_size: int = 10, filters: dict = None, sort_ : dict = None):
+    def select_by_all(
+        self,
+        field_names: list = [],
+        page_num: int = 1,
+        page_size: int = 10,
+        filters: dict = {},
+        sort_: dict = {},
+    ):
         field_str = self._get_field_names_str(field_names)
 
-        start = (page_num - 1)*page_size
+        start = (page_num - 1) * page_size
 
         if sort_ is not None and len(sort_) > 0:
             sort_clause = "ORDER BY "
-            temp = []
             for field, s_order in sort_.items():
-                sort_clause += "{} {}".format(field, s_order) 
+                sort_clause += "{} {}".format(field, s_order)
                 if field != list(sort_.keys())[-1]:
                     sort_clause += ","
             print(sort_clause)
         else:
             sort_clause = ""
 
-        # Check if any filters were provided
-        if filters is not None and len(filters) > 0:
-            # Build the WHERE clause based on the filters
-            where_clause = "WHERE "
-            for field, value in filters.items():
-                # Add each filter condition to the clause
-                where_clause += "{} = '{}'".format(field, value)
-                # Add an AND operator after each condition except for the last one
-                if field != list(filters.keys())[-1]:
-                    where_clause += " AND "
-        else:
-            where_clause = ""
+        where_clause = self._get_where_clause(filters)
 
         sql_query_str = "SELECT {} FROM {} {} {} LIMIT {} OFFSET {}".format(
-            field_str, self.model_class.__tablename__, where_clause, sort_clause, page_size, start
+            field_str,
+            self.model_class.__tablename__,
+            where_clause,
+            sort_clause,
+            page_size,
+            start,
         )
 
         cursor: MySQLModelCursor = self._get_cursor()
